852fd86222a0157114e8b2afbbc994a973fbcf2e
gryzun33 <yakushevao777@gmail.com>
2020-07-23 23:26:59 +0200

    add conditions and cycles

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-" use strict "; 
+' use strict'; 
 
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+// let a, b;
+
+// do {
+//     a = prompt('Один из последних просмотренных фильмов?', ''); 
+//     b = prompt('На сколько оцените его?', '');
+// } while ( a === null || a === '' || b === null || b === '' ||
+//     a.length > 50);
+
+
+
+
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,17 +23,30 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''), 
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''), 
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?','');
+    const b = prompt('На сколько оцените его?','');
 
-console.log( personalMovieDB );
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB[a] = [b];
+        console.log ('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
- 
+if (numberOfFilms < 10) {
+    alert ("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+    alert ("Вы классический зритель"); 
+} else if (numberOfFilms > 30) {
+    alert ("Вы киноман") ;
+} else {
+    alert ("Произошла ошибка");
+}
+
 
 
 
